d0b76c7bd459c885ead84fcfa71a582ed7e67d6a
sergiomaddaleno <madalenofi@esat-alumni.com>
2025-03-24 18:07:12 +0100

    changes

--- proyect/app/src/main/java/com/smf/practica_fede_sergio/Screens/RegistrerScreen.kt
+++ proyect/app/src/main/java/com/smf/practica_fede_sergio/Screens/RegistrerScreen.kt
@@ -6,10 +6,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.smf.practica_fede_sergio.R
 import com.smf.practica_fede_sergio.ViewModel.UserViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -29,7 +30,7 @@ fun RegisterScreen(userViewModel: UserViewModel = viewModel(), onLogin: () -> Un
     ) {
         // Título de la pantalla
         Text(
-            text = "Registro de Usuario",
+            text = stringResource(R.string.Registro),
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier.padding(bottom = 32.dp)
         )
